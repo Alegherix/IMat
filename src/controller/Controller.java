7463309169f5db05eefb97558c1dc6316d2f703a
elinmelander <gusmelelb@student.gu.se>
2019-05-15 15:24:52 +0200

    15may

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javax.swing.text.html.ImageView;
+import javax.xml.soap.Text;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,17 +14,32 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 
+    // Default view
     @FXML ImageView logotype;
     @FXML ImageView shoppingCart;
-
     @FXML Button searchButton;
     @FXML Button myPagesButton;
     @FXML Button cashOutButton;
-
     @FXML TextField searchField;
-
     @FXML Accordion menu;
     @FXML TitledPane offers;
+
+    // Product view
+    @FXML Label productTitle;
+    @FXML ImageView productImage;
+    @FXML Text productInfo;
+    @FXML Label priceTag;
+    @FXML Button plusButton;
+    @FXML Button minusButton;
+    @FXML TextField quantityField;
+
+    //List item
+    @FXML ImageView productImageListView;
+    @FXML Label productNameListView;
+
+    @FXML Button plusButtonListView;
+    @FXML Button minusButtonListView;
+    @FXML TextField quantityFieldListView;
 
     /**
      * Används om vi istället vill binda funktionalitet till listeners i java kod, istället för i .fxml dokumenten
